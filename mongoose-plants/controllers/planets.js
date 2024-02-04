3af99087982d5c4472e31ee8550012683cbaee95
zynbahmed <201901241@student.polytechnic.bh>
2024-02-04 17:01:59 +0300

    adding the views

--- mongoose-plants/controllers/planets.js
+++ mongoose-plants/controllers/planets.js
@@ -6,11 +6,30 @@ const newPlanet = (req, res) => {
 }
 
 const create = async(req, res) => {
+    for(let key in req.body){
+        if(req.body[key] == ''){
+            delete req.body[key]
+        }
+    }
 
+    try {
+        await Planet.create(req.body);
+        res.redirect('planets/new')
+    } catch (error) {
+        console.log(error)
+        res.redirect('planets/new')
+    }
 }
 
 const index = async (req, res) => {
-
+    try {
+        const planets = await Planet.find({})
+        const title = "All Planets"
+        res.render('planets/index', { planets, title });
+    } catch (error){
+        console.log(error)
+        res.redirect('/')
+    }
 }
 
 

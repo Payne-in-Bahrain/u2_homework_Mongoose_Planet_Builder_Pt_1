22a4b87d7d41f65b00e47ad5fc0c5eb2328c3b59
aboodabdo347 <aboodabdo347@gmail.com>
2024-02-05 07:24:49 +0300

    finish the homework

--- planet-builder/models/planet.js
+++ planet-builder/models/planet.js
@@ -1,14 +1,22 @@
+// models/planet.js
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
+
 const planetSchema = new Schema(
   {
     name: { type: String, required: true },
-    climate: { type: String, required: true },
-    population: Number,
+    climate: {
+      type: String,
+      required: true,
+      enum: ['Oceanic', 'Tropical', 'Arctic', 'Desert', 'Rainforest'],
+      default: 'Desert'
+    },
+    population: { type: Number, required: true },
     size: Number
   },
   {
     timestamps: true
   }
 )
+
 module.exports = mongoose.model('Planet', planetSchema)

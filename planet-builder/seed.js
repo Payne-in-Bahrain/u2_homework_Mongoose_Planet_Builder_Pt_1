7594d6135d74d725ccd687ea09d3c9d0c2a1ebc6
Ali Albanna <ali.al-banna@outlook.com>
2024-02-07 21:04:38 +0300

    more seed operations

--- planet-builder/seed.js
+++ planet-builder/seed.js
@@ -1,16 +1,16 @@
 require("dotenv").config();
 require("./config/database");
 
-// const Planet = require("./models/planet");
+const Planet = require("./models/planet");
 
-// const createDummyData = async () => {
-//   try { 
-//     const doc = await Planet.create({});
-//     console.log("🚀 ~ createDummyData ~ doc:", doc)
-//   }catch(error) {
-//     console.log(error);
-//   }
-// }
+const createDummyData = async () => {
+  try { 
+    const doc = await Planet.create({});
+    console.log("🚀 ~ createDummyData ~ doc:", doc)
+  }catch(error) {
+    console.log(error);
+  }
+}
 
 // createDummyData();
 
@@ -18,11 +18,28 @@ const Explorer = require("./models/explorer");
 
 const createExplorer = async () => {
   try {
-    const doc = await Explorer.create({name: "John", age: "35"});
+    const doc = await Explorer.create({name: "Samantha", age: "41"});
     console.log(doc);
   } catch (error) {
     console.log(error);
   }
 }
 
-createExplorer();
+// createExplorer();
+
+const explorerLinkPlanet = async () => {
+  try {
+    const planet = await Planet.findOne({_id: "65bfd7798b6283aa94ac7def"});
+    // console.log(planet.name);
+
+    planet.crew.push("65c3c21b2bee7b3f1bec14c9");
+    planet.crew.push("65c3c1eeee819a04832ddc95")
+    planet.crew.push("65c3c27c5897a60213084f01");
+    
+    planet.save();
+
+  } catch (error) {
+    
+  }
+}
+// explorerLinkPlanet();
